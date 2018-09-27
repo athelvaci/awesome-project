cff597624e88480aec6c2d4e7fbb7c4e64a6d233
athelvaci <athelvaci@gmail.com>
2018-09-28 00:06:08 +0300

    initial commit

---
+++ src/main/java/com/its/awesomeproject/AwesomeProjectApplication.java
@@ -0,0 +1,12 @@
+package com.its.awesomeproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwesomeProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwesomeProjectApplication.class, args);
+    }
+}
